55840cc5de2c1ada1dbc0102ff983b64d206e406
Tomas Garcia <tomas.garcia@zolar.de>
2024-08-26 22:57:44 +0200

    WIP: Button styled

--- components/shared/AddTodo.tsx
+++ components/shared/AddTodo.tsx
@@ -2,17 +2,15 @@ import Form from "../ui/Form";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
 
-
-
 const AddTodo = () => {
   return (
-    <Form >
+    <Form>
       <div className="flex">
         <Input />
         <Button />
       </div>
     </Form>
-  )
-}
+  );
+};
 
-export default AddTodo
+export default AddTodo;
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,7 +1,29 @@
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+"use client";
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  type?: "button" | "submit" | "reset";
+  text: string | ReactNode;
+  onClick?: () => void;
+  actionButton?: boolean;
 }
 
-export default Button
+const Button = ({ type, text, onClick, actionButton }: ButtonProps) => {
+  return (
+    <>
+      <button
+        onClick={onClick}
+        type={type}
+        className={clsx(
+          actionButton && "bg-orange-700 rounded-full p-2 text-white",
+          !actionButton && "bg-orange-700 px-2 text-white"
+        )}
+      >
+        {text}
+      </button>
+    </>
+  );
+};
+
+export default Button;
